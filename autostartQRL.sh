6b9888e31c8e85afa9ab50fdb85e694b9fabbfd0
bidulemachin <30975753+bidulemachin@users.noreply.github.com>
2017-09-04 23:28:47 +0200

    Update autostartQRL.sh
    - fix kill command (replace main.py by start_qrl.py)
    - restore start command to "python start_qrl.py"

--- autostartQRL.sh
+++ autostartQRL.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Killing python..."
-ps -ef | grep "python main.py" | grep -v grep | awk '{print $2}' | xargs kill -9
+ps -ef | grep "python start_qrl.py" | grep -v grep | awk '{print $2}' | xargs kill -9
 
 echo "Updating QRL..."
 cd $HOME/QRL
@@ -10,6 +10,6 @@ echo "Updating QRL python dependencies..."
 sudo pip install -r requirements.txt
 
 echo "Restart QRL node"
-./start_qrl
+python start_qrl.py
 
 $SHELL
