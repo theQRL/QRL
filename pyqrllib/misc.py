f3d80e3cde7061febf584309a68939014141897a
Juan Leni <lenijuan@gmail.com>
2017-09-11 20:10:38 +0200

    xmss initial experiments

--- pyqrllib/misc.py
+++ pyqrllib/misc.py
@@ -2,9 +2,9 @@
 """
 This file provide temporary functions that will later be ported to C++
 """
-from pyqrllib.pyqrllib import ucharVector
+from pyqrllib import ucharVector
 
 
 def get_seed():
     # FIXME: Provide a proper seed based on urandom
-    return ucharVector(32, 0)
+    return ucharVector(32, 0), ucharVector(32, 0), ucharVector(32, 0)
