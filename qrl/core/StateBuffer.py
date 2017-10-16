53afa0cd3d233d9e3899d012cb7900de5b58f6c2
Cyyber <kaushal.forex@gmail.com>
2017-10-16 23:29:43 +0530

    Removed special seed calculation due to epoch change

--- qrl/core/StateBuffer.py
+++ qrl/core/StateBuffer.py
@@ -29,18 +29,13 @@ class StateBuffer:
         return tmp_sl
 
     def update(self, state, parent_state_buffer, block):
-        # epoch mod to know if its the new epoch
-        epoch_mod = block.blockheader.blocknumber % config.dev.blocks_per_epoch
 
         self.set_next_seed(block.blockheader.reveal_hash, parent_state_buffer.next_seed)
         self.hash_chain = deepcopy(parent_state_buffer.hash_chain)
 
-        if epoch_mod == config.dev.blocks_per_epoch - 1:
-            self.next_seed = bin2hstr(hex(self.stake_validators_list.calc_seed()))
+        self.update_stxn_state(state)
 
-        self.update_stxn_state(block, state)
-
-    def update_stxn_state(self, block, state):
+    def update_stxn_state(self, state):
         stxn_state_keys = list(self.stxn_state.keys())
         for addr in stxn_state_keys:
 
