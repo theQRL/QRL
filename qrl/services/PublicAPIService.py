0059c69b45cdc45d196ab012784446749a2ee077
Juan Leni <lenijuan@gmail.com>
2017-10-21 06:48:19 +0200

    Fix import shadowing problem

--- qrl/services/PublicAPIService.py
+++ qrl/services/PublicAPIService.py
@@ -6,18 +6,18 @@ from grpc import StatusCode
 from qrl.core import logger
 from qrl.core.Transaction import Transaction
 from qrl.core.qrlnode import QRLNode
-from qrl.generated.qrl_pb2 import *
+from qrl.generated import qrl_pb2
 from qrl.services.grpcHelper import grpc_exception_wrapper
 
 
-class PublicAPIService(PublicAPIServicer):
+class PublicAPIService(qrl_pb2.PublicAPIServicer):
     # TODO: Separate the Service from the node model
     def __init__(self, qrlnode: QRLNode):
         self.qrlnode = qrlnode
 
-    @grpc_exception_wrapper(GetNodeStateResp, StatusCode.UNKNOWN)
-    def GetNodeState(self, request: GetNodeStateReq, context) -> GetNodeStateResp:
-        info = NodeInfo()
+    @grpc_exception_wrapper(qrl_pb2.GetNodeStateResp, StatusCode.UNKNOWN)
+    def GetNodeState(self, request: qrl_pb2.GetNodeStateReq, context) -> qrl_pb2.GetNodeStateResp:
+        info = qrl_pb2.NodeInfo()
         info.version = self.qrlnode.version
         info.state = self.qrlnode.state
         info.num_connections = self.qrlnode.num_connections
@@ -25,21 +25,21 @@ class PublicAPIService(PublicAPIServicer):
         info.uptime = self.qrlnode.uptime
         info.block_height = self.qrlnode.block_height
         info.stake_enabled = self.qrlnode.staking
-        return GetNodeStateResp(info=info)
+        return qrl_pb2.GetNodeStateResp(info=info)
 
-    @grpc_exception_wrapper(GetKnownPeersResp, StatusCode.UNKNOWN)
-    def GetKnownPeers(self, request: GetKnownPeersReq, context) -> GetKnownPeersResp:
-        known_peers = KnownPeers()
-        known_peers.peers.extend([Peer(ip=p) for p in self.qrlnode.peer_addresses])
-        return GetKnownPeersResp(known_peers=known_peers)
+    @grpc_exception_wrapper(qrl_pb2.GetKnownPeersResp, StatusCode.UNKNOWN)
+    def GetKnownPeers(self, request: qrl_pb2.GetKnownPeersReq, context) -> qrl_pb2.GetKnownPeersResp:
+        known_peers = qrl_pb2.KnownPeers()
+        known_peers.peers.extend([qrl_pb2.Peer(ip=p) for p in self.qrlnode.peer_addresses])
+        return qrl_pb2.GetKnownPeersResp(known_peers=known_peers)
 
-    @grpc_exception_wrapper(GetAddressStateResp, StatusCode.UNKNOWN)
-    def GetAddressState(self, request: GetAddressStateReq, context) -> GetAddressStateResp:
+    @grpc_exception_wrapper(qrl_pb2.GetAddressStateResp, StatusCode.UNKNOWN)
+    def GetAddressState(self, request: qrl_pb2.GetAddressStateReq, context) -> qrl_pb2.GetAddressStateResp:
         address_state = self.qrlnode.get_address_state(request.address)
-        return GetAddressStateResp(state=address_state)
+        return qrl_pb2.GetAddressStateResp(state=address_state)
 
-    @grpc_exception_wrapper(TransferCoinsResp, StatusCode.UNKNOWN)
-    def TransferCoins(self, request: TransferCoinsReq, context) -> TransferCoinsResp:
+    @grpc_exception_wrapper(qrl_pb2.TransferCoinsResp, StatusCode.UNKNOWN)
+    def TransferCoins(self, request: qrl_pb2.TransferCoinsReq, context) -> qrl_pb2.TransferCoinsResp:
         logger.debug("[QRLNode] TransferCoins")
         tx = self.qrlnode.create_send_tx(addr_from=request.address_from,
                                          addr_to=request.address_to,
@@ -48,16 +48,16 @@ class PublicAPIService(PublicAPIServicer):
                                          xmss_pk=request.xmss_pk,
                                          xmss_ots_index=request.xmss_ots_index)
 
-        return TransferCoinsResp(transaction_unsigned=tx.pbdata)
+        return qrl_pb2.TransferCoinsResp(transaction_unsigned=tx.pbdata)
 
-    @grpc_exception_wrapper(TransferCoinsResp, StatusCode.UNKNOWN)
-    def PushTransaction(self, request: PushTransactionReq, context) -> PushTransactionResp:
+    @grpc_exception_wrapper(qrl_pb2.TransferCoinsResp, StatusCode.UNKNOWN)
+    def PushTransaction(self, request: qrl_pb2.PushTransactionReq, context) -> qrl_pb2.PushTransactionResp:
         logger.debug("[QRLNode] PushTransaction")
         tx = Transaction.from_pbdata(request.transaction_signed)
         submitted = self.qrlnode.submit_send_tx(tx)
 
         # FIXME: Improve response type
         # Prepare response
-        answer = PushTransactionResp()
+        answer = qrl_pb2.PushTransactionResp()
         answer.some_response = str(submitted)
         return answer
