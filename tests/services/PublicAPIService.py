14ba146bf96d70f9890ada444ec4b4c99093f259
Juan Leni <lenijuan@gmail.com>
2017-10-27 14:48:09 +0200

    Fixing unit tests

--- tests/services/PublicAPIService.py
+++ tests/services/PublicAPIService.py
@@ -97,7 +97,12 @@ class PublicAPITest(TestCase):
     def test_getKnownPeers(self):
         db_state = Mock(spec=State)
         p2p_factory = Mock(spec=P2PFactory)
+        p2p_factory.nodeState = NodeState()
+        p2p_factory.connections = 23
+        p2p_factory.stake = False
+
         chain = Mock(spec=Chain)
+        chain.height = MagicMock(return_value=0)
 
         qrlnode = QRLNode(db_state)
         qrlnode.set_p2pfactory(p2p_factory)
@@ -107,9 +112,9 @@ class PublicAPITest(TestCase):
         service = PublicAPIService(qrlnode)
         response = service.GetKnownPeers(request=qrl_pb2.GetKnownPeersReq, context=None)
 
-        self.assertEqual(2, len(response.known_peers.peers))
-        self.assertEqual('127.0.0.1', response.known_peers.peers[0].ip)
-        self.assertEqual('192.168.1.1', response.known_peers.peers[1].ip)
+        self.assertEqual(2, len(response.known_peers))
+        self.assertEqual('127.0.0.1', response.known_peers[0].ip)
+        self.assertEqual('192.168.1.1', response.known_peers[1].ip)
 
         print(response)
 
@@ -158,7 +163,7 @@ class PublicAPITest(TestCase):
         qrlnode = QRLNode(db_state)
         qrlnode.set_p2pfactory(p2p_factory)
         qrlnode.set_chain(chain)
-        qrlnode.peer_addresses = ['127.0.0.1', '192.168.1.1']
+        qrlnode._peer_addresses = ['127.0.0.1', '192.168.1.1']
 
         service = PublicAPIService(qrlnode)
 
