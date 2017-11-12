c1a148558372b712618140ad69fc277f564863ca
Juan Leni <lenijuan@gmail.com>
2017-11-12 23:33:39 +0100

    Deleting obsolete code

---
+++ qrl/core/DependencyChecker.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import sys
+
+import os
+import pkg_resources
+
+
+class DependencyChecker:
+    @staticmethod
+    def check():
+        requirements_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                         os.path.pardir,
+                                                         os.path.pardir,
+                                                         "requirements.txt"))
+
+        requirements = []
+        with open(requirements_path, "r") as fp:
+            for line in fp.readlines():
+                if line.startswith("#"):
+                    continue
+                requirements.append(line.strip("\n"))
+
+        try:
+            pkg_resources.require(requirements)
+        except Exception as e:
+            sys.exit("dependencies not satisfied, run [pip3 install -r requirements.txt] first. \n {}".format(e))
