100e0cfb4c4ba21b94f5576c9a797fbb3b480f46
Juan Leni <lenijuan@gmail.com>
2017-11-12 23:43:34 +0100

    Adding unit test

---
+++ tests/services/test_BaseService.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+# Distributed under the MIT software license, see the accompanying
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+from unittest import TestCase
+
+import os
+from mock import Mock
+
+from qrl.core import logger, config
+from qrl.core.State import State
+from qrl.core.qrlnode import QRLNode
+from qrl.generated.qrlbase_pb2 import GetNodeInfoReq
+from qrl.services.BaseService import BaseService
+
+logger.initialize_default(force_console_output=True)
+
+
+class BaseAPITest(TestCase):
+    def __init__(self, *args, **kwargs):
+        super(BaseAPITest, self).__init__(*args, **kwargs)
+
+    def test_getNodeInfo(self):
+        db_state = Mock(spec=State)
+        qrlnode = QRLNode(db_state)
+
+        service = BaseService(qrlnode)
+        response = service.GetNodeInfo(request=GetNodeInfoReq, context=None)
+
+        self.assertEqual(config.dev.version, response.version)
+
+        proto_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                  os.path.pardir,
+                                                  os.path.pardir,
+                                                  "qrl", "protos", "qrl.proto"))
+
+        with open(proto_path, 'r') as content_file:
+            proto_content = content_file.read()
+
+        self.assertEqual(proto_content, response.grpcProto)
