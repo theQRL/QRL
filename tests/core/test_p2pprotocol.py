6b5e14ab288e0cadedf148a21204ca4dbc918e98
jleni <jleni@users.noreply.github.com>
2017-11-14 01:42:37 +0100

    Activating CB test (#574)

--- tests/core/test_p2pprotocol.py
+++ tests/core/test_p2pprotocol.py
@@ -5,6 +5,7 @@ from unittest import TestCase
 
 from google.protobuf.json_format import MessageToJson
 from mock import Mock, MagicMock
+from pyqrllib.pyqrllib import bin2hstr, hstr2bin
 
 from qrl.core import logger
 from qrl.core.Chain import Chain
@@ -26,15 +27,14 @@ class TestP2PProtocol(TestCase):
 
         p.factory.peers_blockheight = MagicMock(return_value=dict())
         p.factory.chain = Mock(spec=Chain)
-        p.factory.buffered_chain.blockheight = MagicMock(return_value=0)
+        p.factory.height = 0
 
         tmp_peer = Mock()
         p.transport.getPeer = MagicMock(return_value=tmp_peer)
 
         data = qrl_pb2.BlockMetaData()
         data.block_number = 3
-        data.hash_header = bytes((53, 130, 168, 57, 183, 215, 120, 178, 209, 30, 194, 223, 221, 58, 72, 124, 62, 148,
-                                  110, 81, 19, 189, 27, 243, 218, 87, 217, 203, 198, 97, 84, 19))
+        data.hash_header = bytes(hstr2bin('3582a839b7d778b2d11ec2dfdd3a487c3e946e5113bd1bf3da57d9cbc6615413'))
 
         jsonData = MessageToJson(data)
         p.CB(jsonData)
