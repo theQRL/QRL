41171da0b932de3c180b56c03856a0d91b0cb3ff
Juan Leni <lenijuan@gmail.com>
2017-11-15 22:24:45 +0100

    Verify python version before starting

--- start_qrl.py
+++ start_qrl.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 # coding=utf-8
-
 # Distributed under the MIT software license, see the accompanying
 # file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+
+import sys
+if sys.version_info < (3, 5):
+    print("This application requires at least Python 3.5")
+    quit(1)
+
 from qrl.core.DependencyChecker import DependencyChecker
 
 DependencyChecker.check()
