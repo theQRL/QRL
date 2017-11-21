14e119e6300303440df4bda3b5780e847a38abda
Andrew Chiw <randomshinichi4869@gmail.com>
2017-11-21 20:24:32 +0000

    Dockerfile.xenial install python-stdeb, nano, vim, VOLUME /travis (needed to share keys.tar) travis/build.sh interactive docker run with same env vars travis/xenial/build.sh BUILD_DIST section, compiles and signs but what to do afterwards?

--- travis/xenial/build.sh
+++ travis/xenial/build.sh
@@ -54,3 +54,27 @@ if [ -n "${DEPLOY:+1}" ]; then
     echo "****************************************************************"
     python3 setup.py sdist
 fi
+
+if [ -n "${BUILD_DIST:+1}" ]; then
+   echo "****************************************************************"
+   echo "****************************************************************"
+   echo "                          BUILD_DIST"
+   echo "****************************************************************"
+   echo "****************************************************************"
+
+   tar xvf keys.tar
+   gpg --import public.gpg || true
+   gpg --import private.gpg || true
+
+   mkdir -p /travis/distro
+   cd /travis/distro
+   pip3 download --no-deps qrl
+   export QRL_SLUG=$(ls qrl-* | sed 's/.tar.gz//')
+   echo "QRL_SLUG: ${QRL_SLUG}"
+   py2dsc --with-python2=False --with-python3=True ${QRL_SLUG}.tar.gz
+   cd deb_dist/${QRL_SLUG}
+   echo 'export DEB_BUILD_OPTIONS=nocheck' >> debian/rules
+   dpkg-buildpackage -k$GPGKEY
+   cd /travis/distro
+
+fi
