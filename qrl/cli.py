7c00912c3684a26e7be00b39d615c084bb8cdd12
Juan Leni <lenijuan@gmail.com>
2017-11-28 23:36:25 +0100

    restructuring CLI

--- qrl/cli.py
+++ qrl/cli.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import click
 import grpc
-from pyqrllib.pyqrllib import mnemonic2bin, hstr2bin
 
 from qrl.core import config
 from qrl.core.Wallet import Wallet
@@ -32,10 +31,10 @@ class CLIContext(object):
 
 @click.group()
 @click.option('--remote', '-r', default=False, is_flag=True, help='connect to remote node')
-@click.option('--host', default='127.0.0.1', help='host address')
-@click.option('--port_pub', default=9009, help='port number (public api)')
-@click.option('--port_adm', default=9008, help='port number (admin api)')
-@click.option('--wallet_dir', default='.', help='local wallet file')
+@click.option('--host', default='127.0.0.1', help='remote host address')
+@click.option('--port_pub', default=9009, help='remote port number (public api)')
+@click.option('--port_adm', default=9008, help='remote port number (admin api)')
+@click.option('--wallet_dir', default='.', help='local wallet dir')
 @click.pass_context
 def qrl(ctx, remote, host, port_pub, port_adm, wallet_dir):
     """
@@ -70,6 +69,10 @@ def _admin_get_wallet(ctx, address):
 
 
 def _print_addresses(ctx, addresses, source_description):
+    if len(addresses) == 0:
+        click.echo('No wallet found at {}'.format(source_description))
+        return
+
     click.echo('Wallet at          : {}'.format(source_description))
     click.echo('{:<8}{:<75}{}'.format('Number', 'Address', 'Balance'))
     click.echo('-' * 95)
@@ -86,27 +89,9 @@ def _print_addresses(ctx, addresses, source_description):
 
 def _public_get_address_balance(ctx, address):
     stub = qrl_pb2_grpc.PublicAPIStub(ctx.obj.channel_public)
-
     getAddressStateReq = qrl_pb2.GetAddressStateReq(address=address)
-    f = stub.GetAddressState.future(getAddressStateReq, timeout=5)
-    getAddressStateResp = f.result(timeout=5)
-
+    getAddressStateResp = stub.GetAddressState(getAddressStateReq, timeout=1)
     return getAddressStateResp.state.balance
-
-
-def _remote_print_wallet_list(ctx):
-    addresses = _admin_get_local_addresses(ctx)
-    _print_addresses(ctx, addresses, ctx.obj.node_public_address)
-
-def _local_print_wallet_list(ctx):
-    config.user.wallet_path = ctx.obj.wallet_dir
-    wallet = Wallet(valid_or_create=False)
-    if len(wallet.address_bundle)==0:
-        click.echo('No wallet found at {}'.format(config.user.wallet_path))
-        return
-
-    addresses = [a.address for a in wallet.address_bundle]
-    _print_addresses(ctx, addresses, config.user.wallet_path)
 
 
 def select_wallet(walletObj):
@@ -127,19 +112,23 @@ def select_wallet(walletObj):
 
 @qrl.command()
 @click.pass_context
-def wallets(ctx):
+def wallet_ls(ctx):
     """
     Lists available wallets
     """
     if ctx.obj.remote:
-        _remote_print_wallet_list(ctx)
+        addresses = _admin_get_local_addresses(ctx)
+        _print_addresses(ctx, addresses, ctx.obj.node_public_address)
     else:
-        _local_print_wallet_list(ctx)
+        config.user.wallet_path = ctx.obj.wallet_dir
+        wallet = Wallet(valid_or_create=False)
+        addresses = [a.address for a in wallet.address_bundle]
+        _print_addresses(ctx, addresses, config.user.wallet_path)
 
 
 @qrl.command()
 @click.pass_context
-def generate(ctx):
+def wallet_gen(ctx):
     """
     Adds an address or generates a new wallet (working directory)
     """
@@ -151,66 +140,78 @@ def generate(ctx):
     wallet = Wallet()
     wallet.append(wallet.get_new_address())
 
-    click.echo('Wallet at          : {}'.format(wallet.wallet_dat_filename))
-    for address_bundle in wallet.address_bundle:
-        click.echo('Wallet Address     : {}'.format(address_bundle.address.decode(), ))
+    addresses = [a.address for a in wallet.address_bundle]
+    _print_addresses(ctx, addresses, config.user.wallet_path)
 
 
-@qrl.command()
-@click.option('--seed-type', type=click.Choice(['hexseed', 'mnemonic']), default='hexseed')
-@click.pass_context
-def recover(seed_type):
-    """
-    Recover Wallet using hexseed or mnemonic (32 words)
-    """
-    seed = click.prompt('Please enter your %s' % (seed_type,))
-    seed = seed.lower().strip()
-
-    if seed_type == 'mnemonic':
-        words = seed.split()
-        if len(words) != 32:
-            print('You have entered %s words' % (len(words),))
-            print('Mnemonic seed must contain only 32 words')
-            return
-        bin_seed = mnemonic2bin(seed)
-    else:
-        if len(seed) != 96:
-            print('You have entered hexseed of %s characters' % (len(seed),))
-            print('Hexseed must be of only 96 characters.')
-            return
-        bin_seed = hstr2bin(seed)
-
-    walletObj = Wallet()
-    addrBundle = walletObj.get_new_address(seed=bin_seed)
-    print('Recovered Wallet Address : %s' % (addrBundle.address.decode(),))
-    for addr in walletObj.address_bundle:
-        if addrBundle.address == addr.address:
-            print('Wallet Address is already in the wallet list')
-            return
-
-    if click.confirm('Do you want to save the recovered wallet?'):
-        walletObj.address_bundle.append(addrBundle)
-        click.echo('Saving...')
-        walletObj.save_wallet()
-        click.echo('Done')
+# @qrl.command()
+# @click.option('--seed-type', type=click.Choice(['hexseed', 'mnemonic']), default='hexseed')
+# @click.pass_context
+# def recover(seed_type):
+#     """
+#     Recover Wallet using hexseed or mnemonic (32 words)
+#     """
+#     seed = click.prompt('Please enter your %s' % (seed_type,))
+#     seed = seed.lower().strip()
+#
+#     if seed_type == 'mnemonic':
+#         words = seed.split()
+#         if len(words) != 32:
+#             print('You have entered %s words' % (len(words),))
+#             print('Mnemonic seed must contain only 32 words')
+#             return
+#         bin_seed = mnemonic2bin(seed)
+#     else:
+#         if len(seed) != 96:
+#             print('You have entered hexseed of %s characters' % (len(seed),))
+#             print('Hexseed must be of only 96 characters.')
+#             return
+#         bin_seed = hstr2bin(seed)
+#
+#     walletObj = Wallet()
+#     addrBundle = walletObj.get_new_address(seed=bin_seed)
+#     print('Recovered Wallet Address : %s' % (addrBundle.address.decode(),))
+#     for addr in walletObj.address_bundle:
+#         if addrBundle.address == addr.address:
+#             print('Wallet Address is already in the wallet list')
+#             return
+#
+#     if click.confirm('Do you want to save the recovered wallet?'):
+#         walletObj.address_bundle.append(addrBundle)
+#         click.echo('Saving...')
+#         walletObj.save_wallet()
+#         click.echo('Done')
 
 
 @qrl.command()
 @click.option('--wallet-idx', default=0, prompt=True)
 @click.pass_context
-def mnemonic(ctx, wallet_idx):
+def wallet_secret(ctx, wallet_idx):
     """
-    Provides the mnemonic words of the address into wallet list.
+    Provides the mnemonic/hexseed of the given address index
     """
-    addresses = _admin_get_local_addresses(ctx)
+    if ctx.obj.remote:
+        addresses = _admin_get_local_addresses(ctx)
 
-    if 0 <= wallet_idx < len(addresses):
-        wallet = _admin_get_wallet(ctx, addresses[wallet_idx])
+        if 0 <= wallet_idx < len(addresses):
+            wallet = _admin_get_wallet(ctx, addresses[wallet_idx])
 
-        click.echo('Wallet Address  : %s' % (wallet.address,))
-        click.echo('Mnemonic        : %s' % (wallet.mnemonic,))
+            click.echo('Wallet Address  : %s' % (wallet.address,))
+            click.echo('Mnemonic        : %s' % (wallet.mnemonic,))
+        else:
+            click.echo('Wallet index not found', color='yellow')
+
     else:
-        click.echo('Wallet index not found', color='yellow')
+        config.user.wallet_path = ctx.obj.wallet_dir
+
+        wallet = Wallet(valid_or_create=False)
+
+        if 0 <= wallet_idx < len(wallet.address_bundle):
+            addr_bundle = wallet.address_bundle[wallet_idx]
+            click.echo('Wallet Address  : %s' % (addr_bundle.address.decode()))
+            click.echo('Mnemonic        : %s' % (addr_bundle.xmss.get_mnemonic()))
+        else:
+            click.echo('Wallet index not found', color='yellow')
 
 
 @qrl.command()
@@ -219,9 +220,34 @@ def mnemonic(ctx, wallet_idx):
 @click.option('--amount', default=0, prompt=True)
 @click.option('--fee', default=0, prompt=True)
 @click.pass_context
-def send(ctx, src, dst, amount, fee):
+def tx_prepare(ctx, src, dst, amount, fee):
     """
-    Transfer coins
+    Request a tx blob from src to dst (unsigned)
+    """
+    pass
+
+
+@qrl.command()
+@click.option('--src', default='', prompt=True)
+@click.option('--dst', default='', prompt=True)
+@click.option('--amount', default=0, prompt=True)
+@click.option('--fee', default=0, prompt=True)
+@click.pass_context
+def tx_sign(ctx, src, dst, amount, fee):
+    """
+    Sign a tx blob
+    """
+
+
+@qrl.command()
+@click.option('--src', default='', prompt=True)
+@click.option('--dst', default='', prompt=True)
+@click.option('--amount', default=0, prompt=True)
+@click.option('--fee', default=0, prompt=True)
+@click.pass_context
+def tx_transfer(ctx, src, dst, amount, fee):
+    """
+    Transfer coins from src to dst
     """
     channel = grpc.insecure_channel(ctx.obj.node_public_address)
     stub = qrl_pb2_grpc.PublicAPIStub(channel)
