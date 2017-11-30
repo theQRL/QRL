e2ce4e44041aa46805d931e00069945367cc1fd0
cyyber <kaushal.forex@gmail.com>
2017-11-30 06:07:20 +0530

    Bug fix + Removing obselete tx_timestamp

--- qrl/core/TransactionPool.py
+++ qrl/core/TransactionPool.py
@@ -1,5 +1,4 @@
 from collections import OrderedDict
-from time import time
 from typing import Union
 
 from qrl.core import config
@@ -14,7 +13,6 @@ class TransactionPool:
         self.pending_tx_pool = []
         self.pending_tx_pool_hash = []
         self.transaction_pool = []  # FIXME: Everyone is touching this
-        self.txhash_timestamp = []  # FIXME: Seems obsolete? Delete?
 
     def add_tx_to_duplicate_pool(self, duplicate_txn: DuplicateTransaction):
         if len(self.duplicate_tx_pool) >= config.dev.transaction_pool_size:
@@ -31,13 +29,9 @@ class TransactionPool:
 
     def add_tx_to_pool(self, tx_class_obj: Union[StakeTransaction, DestakeTransaction]):
         self.transaction_pool.append(tx_class_obj)
-        self.txhash_timestamp.append(tx_class_obj.txhash)
-        self.txhash_timestamp.append(time())
 
     def remove_tx_from_pool(self, tx_class_obj: Union[StakeTransaction, DestakeTransaction]):
         self.transaction_pool.remove(tx_class_obj)
-        self.txhash_timestamp.pop(self.txhash_timestamp.index(tx_class_obj.txhash) + 1)
-        self.txhash_timestamp.remove(tx_class_obj.txhash)
 
     def remove_tx_in_block_from_pool(self, block_obj: Block):
         for protobuf_tx in block_obj.transactions:
