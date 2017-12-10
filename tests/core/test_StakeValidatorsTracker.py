e34a1cfdbf72ff2f498ec152b6b5b9d11c1974ea
cyyber <kaushal.forex@gmail.com>
2017-12-11 04:45:16 +0530

    Temporary Fixes

--- tests/core/test_StakeValidatorsTracker.py
+++ tests/core/test_StakeValidatorsTracker.py
@@ -29,11 +29,12 @@ class TestStakeValidatorsTracker(TestCase):
         stake_transaction = StakeTransaction.create(0, alice_xmss, slave_xmss.pk(), b'1111')
         stake_validators_tracker.add_sv(100, stake_transaction, 1)
 
-        balance = stake_validators_tracker.get_stake_balance(alice_xmss.get_address())
+        balance = stake_validators_tracker.get_stake_balance(alice_xmss.get_address().encode())
         self.assertEqual(100, balance)
 
         total_stake_amount = stake_validators_tracker.get_total_stake_amount()
         self.assertEqual(100, total_stake_amount)
+        stake_validators_tracker.to_json()
 
     def test_add_future_sv(self):
         alice_xmss = get_alice_xmss()
