5a85758225a423cbd0bc14cfcc11e55271213839
Juan Leni <lenijuan@gmail.com>
2017-12-22 15:43:59 +0100

    Removing APIs linked to node's wallet

--- qrl/services/AdminAPIService.py
+++ qrl/services/AdminAPIService.py
@@ -15,19 +15,3 @@ class AdminAPIService(AdminAPIServicer):
     # TODO: Separate the Service from the node model
     def __init__(self, qrlnode: QRLNode):
         self.qrlnode = qrlnode
-
-    @grpc_exception_wrapper(qrl_pb2.GetLocalAddressesResp, StatusCode.UNKNOWN)
-    def GetLocalAddresses(self, request: qrl_pb2.GetLocalAddressesReq, context) -> qrl_pb2.GetLocalAddressesResp:
-        return qrl_pb2.GetLocalAddressesResp(addresses=self.qrlnode.addresses)
-
-    @grpc_exception_wrapper(qrl_pb2.GetWalletResp, StatusCode.UNKNOWN)
-    def GetWallet(self, request: qrl_pb2.GetWalletReq, context) -> qrl_pb2.GetWalletResp:
-        answer = qrl_pb2.GetWalletResp()
-
-        addr_bundle = self.qrlnode.get_address_bundle(request.address)
-        if addr_bundle is not None:
-            answer.wallet.CopyFrom(qrl_pb2.Wallet(address=addr_bundle.address,
-                                                  mnemonic=addr_bundle.xmss.get_mnemonic(),
-                                                  xmss_index=addr_bundle.xmss.get_index()))
-
-        return answer
