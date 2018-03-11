d56ea7ea0d5d78b432043b0735f2c699b00a4c36
cyyber <kaushal.forex@gmail.com>
2018-03-11 10:53:56 +0530

    Changed hardcoded seed to input

--- tools/generate_genesis.py
+++ tools/generate_genesis.py
@@ -1,13 +1,15 @@
 # coding=utf-8
 from __future__ import print_function
 
+from pyqrllib.pyqrllib import hstr2bin
+
 from qrl.generated import qrl_pb2
 from qrl.core import config
 from qrl.core.Block import Block
 from qrl.crypto.xmss import XMSS
 
 
-seed = b'\x01\x03\x00\x8e\xf4\xd7\x00\x05\xc2.\x83P\xceJ\xc9)\xfb\x98+\xff\x84\x12-\x9b]*X\xe96\xd70n\xc9\x0e\x0f\xdd\xb7\x97^\x9c)\xca\xcf\x1fp\x9d\x02\xcb\xdeU\xd4'
+seed = bytes(hstr2bin(input('Enter extended hexseed: ')))
 
 dist_xmss = XMSS.from_extended_seed(seed)
 
