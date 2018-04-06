ea9373c899ade2c0e16c3d0db9e2ec5cdd488c13
Juan Leni <lenijuan@gmail.com>
2018-04-06 11:10:03 +0200

    Removing obsolete test

--- tests/misc/helper_tests.py
+++ tests/misc/helper_tests.py
@@ -7,7 +7,6 @@ from pyqrllib.pyqrllib import bin2hstr
 
 from qrl.core.misc import logger
 from tests.misc import helper
-from tests.misc.helper import qrlnode_with_mock_blockchain
 
 logger.initialize_default()
 
@@ -20,8 +19,3 @@ class TestHelpers(TestCase):
         address = helper.qrladdress('mySeed')
         self.assertEqual('00020080a24d25a75c99077719c6b5077b0ae16cf243f69d142e848075e985dbb28df7fbcd5acf',
                          bin2hstr(address))
-
-    def test_mock_blockchain(self):
-        number_blocks = 200
-        with qrlnode_with_mock_blockchain(number_blocks) as qrlnode:
-            self.assertIsNotNone(qrlnode)
