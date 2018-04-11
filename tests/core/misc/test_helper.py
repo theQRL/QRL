84d6c08e7178112483638b6dba173aaf8d0940ff
Juan Leni <lenijuan@gmail.com>
2018-04-11 09:03:43 +0200

    removing unused variables

--- tests/core/misc/test_helper.py
+++ tests/core/misc/test_helper.py
@@ -31,28 +31,29 @@ class TestHelper(TestCase):
 
     def test_invalid_1(self):
         with self.assertRaisesRegexp(AddressValueError, 'Address cannot be empty'):
-            ip, port = parse_peer_addr('')
+            parse_peer_addr('')
 
     def test_invalid_2(self):
         with self.assertRaisesRegexp(AddressValueError, 'Expected 4 octets in \'abc\''):
-            ip, port = parse_peer_addr('abc')
+            parse_peer_addr('abc')
 
     def test_wrong_port_1(self):
         with self.assertRaisesRegexp(ValueError, 'Invalid Peer Port 192.168.0.1:100000'):
-            ip, port = parse_peer_addr('192.168.0.1:100000')
+            parse_peer_addr('192.168.0.1:100000')
 
     def test_wrong_port_2(self):
         with self.assertRaisesRegexp(ValueError, 'Invalid Peer Port 192.168.0.1:A'):
-            ip, port = parse_peer_addr('192.168.0.1:A')
+            parse_peer_addr('192.168.0.1:A')
 
     def test_wrong_port_3(self):
         with self.assertRaisesRegexp(ValueError, 'Invalid Peer Port 192.168.0.1:-1'):
-            ip, port = parse_peer_addr('192.168.0.1:-1')
+            parse_peer_addr('192.168.0.1:-1')
 
     def test_global_1(self):
         with self.assertRaisesRegexp(ValueError, 'Local Peer IP Found 192.168.0.1:9000'):
-            ip, port = parse_peer_addr('192.168.0.1:9000', check_global=True)
+            parse_peer_addr('192.168.0.1:9000', check_global=True)
 
     def test_global_2(self):
         ip, port = parse_peer_addr('123.123.123.1:9000', check_global=True)
+        self.assertEquals('123.123.123.1', ip)
         self.assertEquals(9000, port)
