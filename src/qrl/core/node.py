1ee68bca33407dc1f4bafb7b5b5baa869a16f447
Juan Leni <lenijuan@gmail.com>
2018-06-20 12:33:30 +0200

    Skip and re-queue events

--- src/qrl/core/node.py
+++ src/qrl/core/node.py
@@ -82,7 +82,7 @@ class POW(ConsensusMechanism):
     def _handler_state_synced(self):
         self.last_pow_cycle = ntp.getTime()
         last_block = self.chain_manager.last_block
-        self.mine_next(last_block)
+        self._mine_next(last_block)
 
     def _handler_state_forked(self):
         pass
@@ -193,7 +193,7 @@ class POW(ConsensusMechanism):
         if not self.miner.isRunning() or self.miner_toggler:
             logger.debug('Mine next called by monitor_miner')
             self.miner_toggler = False
-            self.mine_next(self.chain_manager.last_block)
+            self._mine_next(self.chain_manager.last_block)
         elif self.miner.solutionAvailable():
             self.miner_toggler = True
         else:
@@ -236,7 +236,7 @@ class POW(ConsensusMechanism):
                 logger.debug('try last block')
                 last_block = self.chain_manager.last_block
                 logger.debug('got last block')
-                self.mine_next(last_block)
+                self._mine_next(last_block)
 
             if not result:
                 logger.debug('Block Rejected %s %s', block.block_number, bin2hstr(block.headerhash))
@@ -255,7 +255,7 @@ class POW(ConsensusMechanism):
             return True
         return False
 
-    def mine_next(self, parent_block):
+    def _mine_next(self, parent_block):
         if ntp.getTime() < self.suspend_mining_timestamp:
             return
 
