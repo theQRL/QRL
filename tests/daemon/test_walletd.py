358911d0e9e3016a8f12d8a51406dc7829fa5d08
cyyber <kaushal.forex@gmail.com>
2018-08-22 10:41:42 +0530

    WalletAPI converts binaries into hex string and avoids any base64 encoding by protobuf

--- tests/daemon/test_walletd.py
+++ tests/daemon/test_walletd.py
@@ -488,7 +488,7 @@ class TestWalletD(TestCase):
                 return_value=qrl_pb2.GetTransactionResp(tx=tx, confirmations=10))
             tx, confirmations = walletd.get_transaction(tx_hash='1234')
             self.assertIsNotNone(tx)
-            self.assertEqual(tx.transaction_hash, b'1234')
+            self.assertEqual(tx.transaction_hash, bin2hstr(b'1234'))
             self.assertEqual(confirmations, 10)
 
     def test_get_balance(self):
@@ -531,7 +531,7 @@ class TestWalletD(TestCase):
                 return_value=qrl_pb2.GetBlockResp(block=block))
 
             b = walletd.get_block('001122')
-            self.assertEqual(b.header.hash_header, block.header.hash_header)
+            self.assertEqual(b.header.hash_header, bin2hstr(block.header.hash_header))
             self.assertEqual(b.header.block_number, block.header.block_number)
 
     def test_get_block_by_number(self):
@@ -546,20 +546,14 @@ class TestWalletD(TestCase):
                 return_value=qrl_pb2.GetBlockResp(block=block))
 
             b = walletd.get_block_by_number(1)
-            self.assertEqual(b.header.hash_header, block.header.hash_header)
+            self.assertEqual(b.header.hash_header, bin2hstr(block.header.hash_header))
             self.assertEqual(b.header.block_number, block.header.block_number)
 
     def test_get_address_from_pk(self):
         with set_qrl_dir("wallet_ver1"):
             walletd = WalletD()
-            pk = b'\x01\x02\x00\x16\xec\xb9\xf3\x9b\x9fBu\xd5\xa4\x9e##F\xa1Z\xe2\xfa\x8cP\xa2\x92}\xae' \
-                 b'\xac\x18\x9b\x8c_-\x18\xbcN9\x83\xbdVB\x98\xc4\x9a\xe2\xe7\xfan(\xd4\xb9T\xd8\xcdY9' \
-                 b'\x8f\x12%\xb0\x8daD\x85J\xee\x0e'
-
-            address = b'\x01\x02\x00g\x02F\xb0\x02d6\xb7\x17\xf1\x99\xe3\xecS \xbaj\xb6\x1d^\xdd\xff\x81' \
-                      b'\x1a\xc1\x99\xa9\xe9\xb8q\xd3(\x01x\xb3C'
-
-            walletd._public_stub.GetAddressFromPK = Mock(return_value=qrl_pb2.GetAddressFromPKResp(address=address))
+            pk = '01020016ecb9f39b9f4275d5a49e232346a15ae2fa8c50a2927daeac189b8c5f2d1' \
+                 '8bc4e3983bd564298c49ae2e7fa6e28d4b954d8cd59398f1225b08d6144854aee0e'
 
             address = walletd.get_address_from_pk(pk)
             self.assertEqual(address, 'Q010200670246b0026436b717f199e3ec5320ba6ab61d5eddff811ac199a9e9b871d3280178b343')
