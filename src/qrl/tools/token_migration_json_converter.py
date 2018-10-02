6e19884e99841bedf054f7515734603c5c92b206
cyyber <kaushal.forex@gmail.com>
2018-10-02 23:12:44 +0530

    Moved code to main function

--- src/qrl/tools/token_migration_json_converter.py
+++ src/qrl/tools/token_migration_json_converter.py
@@ -3,19 +3,25 @@ import simplejson as json
 from decimal import Decimal
 from qrl.core import config
 
-json_data = dict()
 
-with open('data/token_migration.csv') as f:
-    data = csv.reader(f, delimiter=',')
+def main():
+    json_data = dict()
 
-    shor_per_quanta = int(config.dev.shor_per_quanta)
-    count = 0
-    for row in data:
-        count += 1
-        json_data[row[0]] = int(Decimal(row[1]) * shor_per_quanta)
+    with open('data/token_migration.csv') as f:
+        data = csv.reader(f, delimiter=',')
 
-    if count != len(json_data.keys()):
-        raise Exception('There must be duplicate address in csv file.')
+        shor_per_quanta = int(config.dev.shor_per_quanta)
+        count = 0
+        for row in data:
+            count += 1
+            json_data[row[0]] = int(Decimal(row[1]) * shor_per_quanta)
 
-with open('data/token_migration.json', 'w') as f:
-    json.dump(json_data, f)
+        if count != len(json_data.keys()):
+            raise Exception('There must be duplicate address in csv file.')
+
+    with open('data/token_migration.json', 'w') as f:
+        json.dump(json_data, f)
+
+
+if __name__ == '__main__':
+    main()
