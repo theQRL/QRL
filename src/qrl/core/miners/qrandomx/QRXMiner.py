a0c20059bf5df26e36fb5e7e2013b0b909bcaed7
cyyber <kaushal.forex@gmail.com>
2020-02-13 00:41:44 +0530

    Removed unused import

--- src/qrl/core/miners/qrandomx/QRXMiner.py
+++ src/qrl/core/miners/qrandomx/QRXMiner.py
@@ -2,7 +2,6 @@
 # Distributed under the MIT software license, see the accompanying
 # file LICENSE or http://www.opensource.org/licenses/mit-license.php.
 import copy
-import threading
 
 from pyqrandomx.pyqrandomx import QRXMiner, SOLUTION
 
