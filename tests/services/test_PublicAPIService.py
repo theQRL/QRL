70055c652cc691163514437050670cca5fa85007
cyyber <kaushal.forex@gmail.com>
2020-02-22 18:32:50 +0530

    Fix: Unit Tests for get_address_state API

--- tests/services/test_PublicAPIService.py
+++ tests/services/test_PublicAPIService.py
@@ -178,8 +178,7 @@ class TestPublicAPI(TestCase):
             self.assertEqual(alice_xmss.address, response.state.address)
             self.assertEqual(25, response.state.nonce)
             self.assertEqual(10, response.state.balance)
-            count = int(ceil((2 ** optimized_address_state.height) / 8))
-            self.assertEqual([b'\x00'] * count, response.state.ots_bitfield)
+            self.assertEqual([b'\x00'] * config.dev.ots_bitfield_size, response.state.ots_bitfield)
             self.assertEqual([], response.state.transaction_hashes)
 
     def test_getObject(self):
