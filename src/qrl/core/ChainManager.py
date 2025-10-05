3e6a4f005edfafdad1b9b3252853ce2adb9ed9c7
JP Lomas <jp@theqrl.org>
2025-10-05 13:01:06 +0100

    ChainManager edge case
    Fix NoneType errors when blocks are missing during fast network scenarios

--- src/qrl/core/ChainManager.py
+++ src/qrl/core/ChainManager.py
@@ -333,15 +333,21 @@ class ChainManager:
 
         # If parent block belongs to main chain, then seed block will also be in the main chain
         prev_mainchain_block = self.get_block_by_number(blockheader.block_number - 1)
-        if prev_mainchain_block.headerhash == blockheader.prev_headerhash:
+        if prev_mainchain_block is not None and prev_mainchain_block.headerhash == blockheader.prev_headerhash:
             return self.get_block_by_number(seed_height)
 
         prev_block = self.get_block(blockheader.prev_headerhash)
+        if prev_block is None:
+            # If we can't find the previous block, return None to indicate failure
+            return None
         while prev_block.block_number > seed_height:
             prev_mainchain_block = self.get_block_by_number(prev_block.block_number)
-            if prev_mainchain_block.headerhash == prev_block.headerhash:
+            if prev_mainchain_block is not None and prev_mainchain_block.headerhash == prev_block.headerhash:
                 return self.get_block_by_number(seed_height)
             prev_block = self.get_block(prev_block.prev_headerhash)
+            if prev_block is None:
+                # If we can't find the previous block during traversal, return None
+                return None
 
         return prev_block
 
@@ -362,6 +368,10 @@ class ChainManager:
             mining_blob = blockheader.mining_blob(dev_config)
             qn = Qryptonight()
             seed_block = self.get_seed_block(blockheader)
+            if seed_block is None:
+                if enable_logging:
+                    logger.warning("Could not find seed block for validation")
+                return False
 
             if enable_logging:
                 logger.debug('-----------------START--------------------')
